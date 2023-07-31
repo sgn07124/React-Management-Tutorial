83a4099ec994d7bc6a1eec258905607947bf2bf3
kimkyungho <sgn07124@gmail.com>
2023-07-31 15:31:25 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const customers = [
     'job' : '대학생'
   },
   {
-    'id' : 1,
+    'id' : 2,
     'image' : 'https://placeimg.com/64/64/1',
     'name' : '나동빈',
     'birthday' : '961212',
@@ -20,7 +20,7 @@ const customers = [
     'job' : '대학생'
   },
   {
-    'id' : 1,
+    'id' : 3,
     'image' : 'https://placeimg.com/64/64/2',
     'name' : '김경호',
     'birthday' : '000221',
@@ -33,30 +33,20 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Customer  // components 폴더 내의 Customer.js를 import로 불러와서 그것을 App.js 안에서 출력할 수 있도록 만듬
-          id = {customers[0].id}
-          image = {customers[0].image}
-          name = {customers[0].name}
-          birthday = {customers[0].birthday}
-          gender = {customers[0].gender}
-          job = {customers[0].job}
-        />
-        <Customer  // components 폴더 내의 Customer.js를 import로 불러와서 그것을 App.js 안에서 출력할 수 있도록 만듬
-          id = {customers[1].id}
-          image = {customers[1].image}
-          name = {customers[1].name}
-          birthday = {customers[1].birthday}
-          gender = {customers[1].gender}
-          job = {customers[1].job}
-        />
-        <Customer  // components 폴더 내의 Customer.js를 import로 불러와서 그것을 App.js 안에서 출력할 수 있도록 만듬
-          id = {customers[2].id}
-          image = {customers[2].image}
-          name = {customers[2].name}
-          birthday = {customers[2].birthday}
-          gender = {customers[2].gender}
-          job = {customers[2].job}
-        />
+        {
+          customers.map(c => {
+            return (
+            <Customer
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+            )
+          })
+        }
       </div>
     );
   }
